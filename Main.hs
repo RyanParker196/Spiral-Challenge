f181448ce9c3e1f64f8db58e0c7997835cefb1bf
RyanParker196 <46787914+RyanParker196@users.noreply.github.com>
2020-06-14 16:46:50 -0400

    Fixed print format and bug with top right corner element dissaperaing for n>49

--- Main.hs
+++ Main.hs
@@ -4,19 +4,19 @@ isqrt :: Int -> Int
 isqrt = floor . sqrt . fromIntegral
 
 getX :: Int -> Int
-getX n = let dim = isqrt n in (topRight n (dim)) - dim
+getX n = n - isqrt n
 
 getY :: Int -> Int
-getY n = n - 1
+getY n = n + 1
 
 buildMid :: Int -> Int -> Int -> [[Int]] -> Int -> [[Int]]
-buildMid n x y oGmid prevN = if x == 0 then buildMid n (getX n) (getY n) oGmid prevN else
+buildMid n x y oGmid prevN = if x == 0 then buildMid n (getX n) (prevN + 1) oGmid prevN else
     case oGmid of
     []          -> error "fuck"
     [r]         -> [[x] ++ r ++ [y]]
     (r : rs)    -> if y == n 
-        then [[x],r,[y]] ++ (buildMid n (x - 1) (prevN + 1) rs prevN)
-        else [[x],r,[y]] ++ (buildMid n (x - 1) (y + 1) rs prevN)
+        then ([x] ++ r ++ [y]) : buildMid n (x - 1) (prevN + 1) rs prevN
+        else ([x] ++ r ++ [y]) : buildMid n (x - 1) (y + 1) rs prevN
 
 topRight :: Int -> Int -> Int
 topRight n dim = let temp = dim `div` 2 in n - (temp - 1)
@@ -26,9 +26,9 @@ build n = case n of
     1 -> [ [ 1 ] ]
     n -> 
         let dim = isqrt n
-            prevN = ((dim) - 2) * ((dim) - 2)
+            prevN = (dim - 2) * (dim - 2)
             oGmid = build prevN
-            tpRight = topRight n dim
+            tpRight = n
             top = [(tpRight - (dim - 1)) .. tpRight]
             mid = buildMid n 0 0 oGmid prevN
             btLeft = tpRight - 2 * (dim - 1)
@@ -36,11 +36,9 @@ build n = case n of
         in [top] ++ mid ++ [bot]
 
 printMatrix :: [[Int]] -> String
-printMatrix ans = case ans of
+printMatrix matrix = case matrix of
     []       -> ""
-    (a : as) -> case a of
-        [] -> ""
-        (a' : as') -> show a' ++ ", " ++ (printMatrix [as']) ++ printMatrix as
+    (r : rs) -> show r ++ "\n" ++ printMatrix rs
 
 main :: IO ()
 main = do
@@ -48,4 +46,6 @@ main = do
     let z = read n
         ans = build z 
         fnl = printMatrix ans
-        in if z /= (isqrt z) * (isqrt z) || z `mod` 2 == 0 then error "Must enter odd perfect square" else putStrLn fnl
+        in if z /= isqrt z * isqrt z || z `mod` 2 == 0 
+            then putStrLn "Must enter odd perfect square"
+            else putStrLn fnl
